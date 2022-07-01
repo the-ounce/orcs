d2c217396836782ba051352917a11324f00aa65a
Mykyta Havrylenko <gavverment@gmail.com>
2022-07-01 19:29:25 +0300

    Initial Commit

---
+++ orcs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  orcs
+//
+//  Created by Mykyta Havrylenko on 01.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
